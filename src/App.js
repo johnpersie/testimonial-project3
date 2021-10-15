6ebcbd19d1d35324755d624c75fb32ce88f60b7d
Buokem <talk2chibu@gmail.com>
2021-10-15 14:28:17 +0100

    Created story page 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "../src/sass/App.scss";
 import Navbar from "./components/Navbar";
 import LandingPage from "./components/LandingPage";
+import StoryPage1 from "./components/StoryPage1";
+// import Grid from "./components/Grid";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <LandingPage />
+      <StoryPage1 />
+      {/* <Grid /> */}
     </div>
   );
 }
