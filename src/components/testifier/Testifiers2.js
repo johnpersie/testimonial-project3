c3d36f250f688ab6738ac15180fed674b2aedcdc
johnpersie <83553199+johnpersie@users.noreply.github.com>
2021-10-17 20:19:58 +0100

    creating project initial commit

---
+++ src/components/testifier/Testifiers2.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react'
+
+class Testifiers2 extends Component {
+    state = {
+        contents: [
+            {
+                id: "1",
+                avatar_pic: "avatar2-pics/Ellipse 22.png",
+                content: "Temi Obadofin",
+                title: "vendor",
+                textContent: <p>
+                                Aliqua id fugiat nostrud irure ex duis ea quis id quis ad et. 
+                                Sunt qui esse pariatur duis deserunt mollit dolore cillum minim tempor enim. 
+                                Elit aute irure tempor cupidatat incididunt sint deserunt ut voluptate aute id deserunt nisi. 
+                                Aliqua id fugiat nostrud irure ex duis ea quis id quis ad et. Sunt qui esse pariatur duis deserunt mollit dolore cillum minim tempor enim.
+                            </p>
+            },
+            {
+                id: "2",
+                avatar_pic: "avatar2-pics/Ellipse 23.png",
+                content: "Promise Ejiofor",
+                title: "vendor",
+                textContent: <p>
+                                Aliqua id fugiat nostrud irure ex duis ea quis id quis ad et. 
+                                Sunt qui esse pariatur duis deserunt mollit dolore cillum minim tempor enim. 
+                                Elit aute irure tempor cupidatat incididunt sint deserunt ut voluptate aute id deserunt nisi.
+                                Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
+                            </p>
+            },
+            {
+                id: "3",
+                avatar_pic: "avatar2-pics/Ellipse 24.png",
+                userName: "Feyisola Arinola",
+                title: "vendor",
+                textContent: <p>
+                                Aliqua id fugiat nostrud irure ex duis ea quis id quis ad et. 
+                                Sunt qui esse pariatur duis deserunt mollit dolore cillum minim tempor enim. 
+                                Elit aute irure tempor cupidatat incididunt sint deserunt ut voluptate aute id deserunt nisi. 
+                                Elit aute irure tempor cupidatat incididunt sint deser unt ut voluptate aute id deserunt nisi. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.   
+                            </p>
+            },
+            {
+                id: "4",
+                avatar_pic: "avatar2-pics/Ellipse 22b.png",
+                content: "Karen Ibeh",
+                title: "vendor",
+                textContent: <p>
+                                Aliqua id fugiat nostrud irure ex duis ea quis id quis ad et. 
+                                Sunt qui esse pariatur duis deserunt mollit dolore cillum minim tempor enim. 
+                                Elit aute irure tempor cupidatat incididunt sint deserunt ut voluptate aute id deserunt nisi. 
+                                Aliqua id fugiat nostrud irure ex duis ea quis id quis ad et. Sunt qui esse pariatur duis deserunt mollit dolore cillum minim tempor enim.
+                            </p>
+            },
+            {
+                id: "5",
+                avatar_pic: "avatar2-pics/Ellipse 23b.png",
+                content: "Oluchi Uzo",
+                title: "vendor",
+                textContent: <p>
+                                Aliqua id fugiat nostrud irure ex duis ea quis id quis ad et. 
+                                Sunt qui esse pariatur duis deserunt mollit dolore cillum minim tempor enim. Elit aute irure tempor cupidatat incididunt sint deser <br /> unt ut voluptate aute id deserunt nisi. 
+                                Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. 
+                                Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
+
+
+                            </p>
+            },
+            {
+                id: "6",
+                avatar_pic: "avatar2-pics/Ellipse 24b.png",
+                content: "Amos Okafor",
+                title: "vendor",
+                textContent: <p>
+                                Aliqua id fugiat nostrud irure ex duis ea quis id quis ad et. 
+                                Sunt qui esse pariatur duis deserunt mollit dolore cillum minim tempor enim. 
+                                Elit aute irure tempor cupidatat incididunt sint deserunt ut voluptate aute id deserunt nisi. 
+                                Elit aute irure tempor cupidatat incididunt sint deser unt ut voluptate aute id deserunt nisi. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.
+                            </p>
+            }
+
+        ]
+    }
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Testifiers2
