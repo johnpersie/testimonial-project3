2c2d04318e27b3a60e9d9c0a9e647b3a80262225
johnpersie <83553199+johnpersie@users.noreply.github.com>
2021-10-17 09:56:20 +0100

    created the Testifieritem and testifiers js file

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import LandingPage from "./components/LandingPage";
 import StoryPage1 from "./components/StoryPage1";
 import StoryPage2 from "./components/StoryPage2";
+import Testifiers from "./components/testifier/Testifiers";
 // import Grid from "./components/Grid";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Navbar />
       <LandingPage />
       <StoryPage1 />
+      <Testifiers />
       <StoryPage2 />
       {/* <Grid /> */}
     </div>
