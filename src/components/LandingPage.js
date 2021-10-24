db15976824b9f35515eeef21e7475b926927a994
Buokem <talk2chibu@gmail.com>
2021-10-24 13:26:37 +0100

    Refactoring thesite to make it mobile responsive

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -4,7 +4,7 @@ import img1 from "../img/Testimonial image 1.png";
 const LandingPage = () => {
   return (
     <div className="landing-page">
-      <div className="landing-page__left-container">
+      <div className="landing-page__writeup-container">
         <h1 className="landing-page__heading">
           <span style={{ display: "block" }}>Amazing</span> Experiences from Our
           Wonderful Customers
@@ -14,12 +14,14 @@ const LandingPage = () => {
           your stories with us too.
         </p>
       </div>
-      <div className="landing-page__elipse">
-        <img
-          className="landing-page__testimonial-img-1"
-          src={img1}
-          alt="testimonial"
-        />
+      <div className="landing-page__graphics-container">
+        <div className="landing-page__elipse">
+          <img
+            className="landing-page__testimonial-img-1"
+            src={img1}
+            alt="testimonial"
+          />
+        </div>
       </div>
     </div>
   );
