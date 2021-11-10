d7c956bc64753d726e5ec6414c8d6394d34a5e31
Buokem <talk2chibu@gmail.com>
2021-11-10 23:38:54 +0100

    fixed the navbar

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -5,50 +5,51 @@ import logo from "../img/WhitewatersEngineeringLimited.png";
 const LandingPage = () => {
   return (
     <div className="landing-page">
-      <div className="header">
-        <div className="header__logo-container">
-          <img className="header__logo" src={logo} alt="" />
-          <p className="header__logo-text">Whitewaters Engineering Limited</p>
+      <div className="landing-page__top-container">
+        <div className="header">
+          <div className="header__logo-container">
+            <img className="header__logo" src={logo} alt="" />
+            <p className="header__logo-text">Whitewaters Engineering Limited</p>
+          </div>
+          <ul>
+            <li>
+              <a href="/home" className="header__home">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="/about-us" className="header__about">
+                about us
+              </a>
+            </li>
+            <li>
+              <a href="/our-services" className="header__services">
+                our services
+              </a>
+            </li>
+            <li>
+              <a href="/contact-us" className="header__contact">
+                contact us
+              </a>
+            </li>
+          </ul>
         </div>
-        <ul>
-          <li>
-            <a href="/home" className="header__home">
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="/about-us" className="header__about">
-              about us
-            </a>
-          </li>
-          <li>
-            <a href="/our-services" className="header__services">
-              our services
-            </a>
-          </li>
-          <li>
-            <a href="/contact-us" className="header__contact">
-              contact us
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div className="landing-page__main-container">
-        <div className="landing-page__writeup-container">
-          <h1 className="landing-page__heading">
-            <span style={{ display: "block" }}>Innovation</span> Experience
-            Excellence{" "}
-          </h1>
-          <p className="landing-page__here">
-            Power and Energy Consultants and Contractors
-          </p>
-        </div>
-        <div className="landing-page__graphics-container">
-          <img
-            className="landing-page__testimonial-img-1"
-            src={img1}
-            alt="testimonial"
-          />
+        <div className="landing-page__main-container">
+          <div className="landing-page__writeup-container">
+            <h1 className="landing-page__heading">
+              Innovation, Experience, Excellence
+            </h1>
+            <p className="landing-page__here">
+              Power and Energy Consultants and Contractors
+            </p>
+          </div>
+          <div className="landing-page__graphics-container">
+            <img
+              className="landing-page__testimonial-img-1"
+              src={img1}
+              alt="testimonial"
+            />
+          </div>
         </div>
       </div>
     </div>
