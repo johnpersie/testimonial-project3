e4d5bb5416e8da1f5da3a5fd0dc6d852049c83d6
johnpersie <83553199+johnpersie@users.noreply.github.com>
2021-11-10 02:06:35 +0100

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import "../src/sass/App.scss";
-import HeaderBar from "./components/HeaderBar";
-import Navbar from "./components/Navbar";
-import LandingPage from "./components/LandingPage";
-import StoryPage1 from "./components/StoryPage1";
-import StoryPage2 from "./components/StoryPage2";
-import Testifiers from "./components/testifier/Testifiers";
-import CallToAction from "./components/CallToAction";
-import Footer from "./components/Footer";
-import Testifiers2 from "./components/testifier/Testifiers2";
+import AboutUs from "./components/AboutUs";
+import OurCoreValues from "./components/OurCoreValues";
 
 function App() {
   return (
     <div className="App">
-      <HeaderBar />
-      <Navbar />
-      <LandingPage />
-      <StoryPage1 />
-      <Testifiers />
-      <StoryPage2 />
-      <Testifiers2 />
-      <CallToAction />
-      <Footer />
+      <AboutUs />
+      <OurCoreValues />
     </div>
   );
 }
---
+++ src/components/AboutUs.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <div className="aboutUs">
+      <div className="row">
+        <div className="aboutUs__textbox">
+          <h3>Our Goal, Vision & Mission</h3>
+          <h1>Goal</h1>
+          <p>
+            We are out to create a strong niche in the power and energy
+            industrial sector by providing a business ease though our S.M.A.R.T
+            approach for prospective and existing power and energy investors as
+            well as providing tailored power and energy solution services to
+            various industry players and easing the power and energy hurdles
+            they face in the most efficient and effective manner.
+          </p>
+          <h1>Mission</h1>
+          <p>
+            We are on a mission to provide first class power and energy solution
+            services targeted at promoting industrialization of the nation via
+            easy access to power and energy to our every individual client’s
+            achievable dream and desire through strong synergism with our sister
+            partners.
+          </p>
+          <h1>Vision</h1>
+          <p>
+            Whitewater Engineering Limited vision is to be a one-stop topnotch
+            power and energy company that shall be most preferred by
+            industries/businesses seeking for a better and effective guidance on
+            easy access, efficient management and anything energy and power.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AboutUs;
