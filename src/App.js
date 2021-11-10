32204e297b0eb33f5f3d4b5540f5c6e07351eca3
Buokem <talk2chibu@gmail.com>
2021-11-10 02:12:37 +0100

    Deleted unimportant files and code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import "../src/sass/App.scss";
 import LandingPage from "./components/LandingPage";
-import StoryPage1 from "./components/StoryPage1";
-import StoryPage2 from "./components/StoryPage2";
-import Testifiers from "./components/testifier/Testifiers";
-// import CallToAction from "./components/CallToAction";
 import Footer from "./components/Footer";
-import Testifiers2 from "./components/testifier/Testifiers2";
 import Partners from "./components/Partners";
 
 function App() {
   return (
     <div className="App">
       <LandingPage />
-      <StoryPage1 />
-      <Testifiers />
-      <StoryPage2 />
-      <Testifiers2 />
-      {/* <CallToAction /> */}
       <Partners />
       <Footer />
     </div>
