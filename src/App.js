6aa80d2e3be90df863f61e7a378be1724777de58
Buokem <talk2chibu@gmail.com>
2021-11-13 23:58:07 +0100

    Modified the core-value section of the website

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import "../src/sass/App.scss";
 import AboutUs from "./components/AboutUs";
-import OurCoreValues from "./components/OurCoreValues";
+// import OurCoreValues from "./components/OurCoreValues";
 import LandingPage from "./components/LandingPage";
 import Footer from "./components/Footer";
 import Partners from "./components/Partners";
 import OurServices from "./components/OurServices";
+import CoreValues from "./components/CoreValues";
 
 function App() {
   return (
     <div className="App">
       <LandingPage />
       <AboutUs />
-      <OurCoreValues />
+      <CoreValues />
+      {/* <OurCoreValues /> */}
       <OurServices />
       <Partners />
       <Footer />
