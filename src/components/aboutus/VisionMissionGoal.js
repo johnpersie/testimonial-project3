3af209d97944eea4bcce838d470bee8bc49e6a0d
johnpersie <83553199+johnpersie@users.noreply.github.com>
2021-11-19 17:53:30 +0100

    built the aboutpage: history, vision and  our core values

---
+++ src/components/aboutus/VisionMissionGoal.js
@@ -0,0 +1,62 @@
+import React from "react";
+
+const VisionMissionGoal = () => {
+  return (
+    <section className="vmg">
+      <div className="vmg__wrapper">
+        <div className="vision__container">
+          <div className="vision__content">
+            <div className="vision__content__card">
+              {/* <img src="" alt="" className="vision__content__card--img" /> */}
+            </div>
+            <h1>Vision</h1>
+            <p>
+              to be a one-stop topnotch power and energy company that shall be
+              most preferred by industries/businesses seeking for a better and
+              effective guidance on easy access, efficient management and
+              anything energy and power.
+            </p>
+          </div>
+        </div>
+        <div className="vmg__mission">
+          <div className="mission__container">
+            <div className="mission__content">
+              <div className="mission__content__card">
+                {/* <img src="" alt="" className="mission__content__card--img" /> */}
+              </div>
+              <h1>Mission</h1>
+              <p>
+                to provide first class power and energy solution services
+                targeted at promoting industrialization of the nation via easy
+                access to power and energy to our every individual client’s
+                achievable dream and desire through strong synergism with our
+                sister partners.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="vmg__goal">
+          <h1>Goal</h1>
+          <div className="goal__container">
+            <div className="goal__content">
+              <div className="goal__content__card">
+                {/* <img src="" alt="" className="goal__content__card--img"> */}
+                <p>
+                  We are out to create a strong niche in the power and energy
+                  industrial sector by providing a business ease though our
+                  S.M.A.R.T approach for prospective and existing power and
+                  energy investors as well as providing tailored power and
+                  energy solution services to various industry players and
+                  easing the power and energy hurdles they face in the most
+                  efficient and effective manner
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default VisionMissionGoal;
